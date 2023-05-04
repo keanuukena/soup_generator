2177d4a7df7ad91c0ff6121307efa1f88f5939b3
keanuukena <keanuukena@mail.weber.edu>
2023-05-04 14:48:31 -0600

    inventory_sort

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ Q - Quit
 			print(menu)
 
 #operator commands choice that gives you items to make the game easy to test.
-		elif choice == "OPERATOR_COMMANDS_69":
+		elif choice == "OPERATOR COMMANDS 69":
 			print("you fly up off the ground, activating cheats. The dragon stands no chance now.")
 			bag_space += 100
 			bag.append("dank shroom")
@@ -168,6 +168,7 @@ Q - Quit
 #this is the bag check choice, it just tells you what you have in your bag using len() and a for loop.
 		elif choice == "B":
 			if len(bag) > 0:
+				bag.sort()
 				bag_count = 0
 				for i in bag:
 					bag_count += 1
@@ -415,6 +416,7 @@ You saved your sheep! Grandma would be proud.
 #It also ensures you don't waste tier 2 ingredients, (because you can't get them back), upgrades your bag, drops worthless items, etc.
 		elif choice == "U":
 			if len(bag) > 0:
+				bag.sort()
 				print("These are your items...")
 				for i in bag:
 					print(f"* {i}")
@@ -429,176 +431,173 @@ You saved your sheep! Grandma would be proud.
 					except ValueError:
 						print("That wasn't in your bag. (Maybe a spelling error?)")
 				if "potion" in use_item and use_item != "n":
-					if use_item == "speed potion":
-						if room_name == "cave" or room_name == "thicket" or room_name == "forest":
-							bag.remove("speed potion")
-							print("Your heartbeat beats super fast. Nothing can stop you. Except for the obstacle you just ran into. Oof.")
-						elif room_name == "marshes" or room_name == "farms" or room_name == "cove" or room_name == "tower":
-							bag.remove("speed potion")
-							print("You zoom around the place, but this doesn't seem to help. The problem with being faster than light is you can only live in darkness.")
-						elif room_name == "grove":
-							bag.remove("speed potion")
-							print("You try to speed past the bear, juke it and break its ankles, but huge grizzly claws are great at swatting pests like you. Ouch. It also ate your dank shrooms.")
-							for i in bag:
-								if i == "dank shroom":
-									bag.remove("dank shroom")
-						else:
-							if mega_wheats == False:
-								print("You zoom across the wide open plane, frolicking in the meadows. Oh hey, you found the legendary mega wheats! You've only ever eaten the mini kind.")
-								bag_space += 1
+					if use_item in bag:
+						if use_item == "speed potion":
+							if room_name == "cave" or room_name == "thicket" or room_name == "forest":
 								bag.remove("speed potion")
-								bag.append("mega wheats ★")
-								mega_wheats = True
-							else:
+								print("Your heartbeat beats super fast. Nothing can stop you. Except for the obstacle you just ran into. Oof.")
+							elif room_name == "marshes" or room_name == "farms" or room_name == "cove" or room_name == "tower":
 								bag.remove("speed potion")
 								print("You zoom around the place, but this doesn't seem to help. The problem with being faster than light is you can only live in darkness.")
-
-					if use_item == "fire potion":
-						if room_name == "cave" or room_name == "woods" or room_name == "grove":
-							bag.remove("fire potion")
-							print("Hot, stuffy smoke quickly fills the space and you're forced to retreat!")
-						if room_name == "marshes" or room_name == "fields" or room_name == "farms" or room_name == "tower":
-							bag.remove("fire potion")
-							print("The arson is great, but it doesn't seem to help in this situation. Maybe don't be so HOT-headed. heh heh..")
-						if room_name == "cove":
-							bag.remove("fire potion")
-							print("Congratulations! Your signal fire has attracted pirates to the cove! Unfortunately, the booty-ticklin crew is swift to plunder all of your fuzz-butts.")
-							for i in bag:
-								if i == "fuzz-butt":
-									bag.remove("fuzz-butt")
-						if room_name == "thicket":
-							if stick_bug == False:
-								bag.remove("fire potion")
-								print("You torch the thicket, blasting through as 'how bad could I possibly be' plays in your head. Hey, you found a dancing stick bug on the other side.")
-								bag_space += 2
-								bag.append("stick bug ★")
-								stick_bug = True
+							elif room_name == "grove":
+								bag.remove("speed potion")
+								print("You try to speed past the bear, juke it and break its ankles, but huge grizzly claws are great at swatting pests like you. Ouch. It also ate your dank shrooms.")
+								while "dank shroom" in bag:
+									bag.remove("dank shroom")
 							else:
+								if mega_wheats == False:
+									print("You zoom across the wide open plane, frolicking in the meadows. Oh hey, you found the legendary mega wheats! You've only ever eaten the mini kind.")
+									bag_space += 1
+									bag.remove("speed potion")
+									bag.append("mega wheats ★")
+									mega_wheats = True
+								else:
+									bag.remove("speed potion")
+									print("You zoom around the place, but this doesn't seem to help. The problem with being faster than light is you can only live in darkness.")
+
+						if use_item == "fire potion":
+							if room_name == "cave" or room_name == "woods" or room_name == "grove":
+								bag.remove("fire potion")
+								print("Hot, stuffy smoke quickly fills the space and you're forced to retreat!")
+							if room_name == "marshes" or room_name == "fields" or room_name == "farms" or room_name == "tower":
 								bag.remove("fire potion")
 								print("The arson is great, but it doesn't seem to help in this situation. Maybe don't be so HOT-headed. heh heh..")
+							if room_name == "cove":
+								bag.remove("fire potion")
+								print("Congratulations! Your signal fire has attracted pirates to the cove! Unfortunately, the booty-ticklin crew is swift to plunder all of your fuzz-butts.")
+								while "fuzz-butt" in bag:
+									bag.remove("fuzz-butt")
+							if room_name == "thicket":
+								if stick_bug == False:
+									bag.remove("fire potion")
+									print("You torch the thicket, blasting through as 'how bad could I possibly be' plays in your head. Hey, you found a dancing stick bug on the other side.")
+									bag_space += 2
+									bag.append("stick bug ★")
+									stick_bug = True
+								else:
+									bag.remove("fire potion")
+									print("The arson is great, but it doesn't seem to help in this situation. Maybe don't be so HOT-headed. heh heh..")
 
-					if use_item == "growth potion":
-						if room_name == "cave" or room_name == "thicket" or room_name == "tower":
-							bag.remove("growth potion")
-							print("You grow tall enough to play basketball. Unfortunately, your size gets you stuck until it wears off. Kind of a BIG problem. heh heh..")
-						if room_name == "marshes" or room_name == "cove" or room_name == "grove" or room_name == "fields":
-							bag.remove("growth potion")
-							print("You grow extraordinarily tall, but it's lonely up here. What would grandma think if she saw how fat you've gotten?")
-						if room_name == "grove":
-							bag.remove("growth potion")
-							print("You grow taller than the grove, tall enough to squish a bear! You can't see it in the trees, though.")
-						if room_name == "woods":
-							bag.remove("growth potion")
-							print("Giant butterflies are cool and all, but not when they eat all of your magic tomatoes!")
-							for i in bag:
-								if i == "magic tomato":
-									bag.remove("magic tomato")
-						if room_name == "farms":
-							if deuce_beans == False:
+						if use_item == "growth potion":
+							if room_name == "cave" or room_name == "thicket" or room_name == "tower":
 								bag.remove("growth potion")
-								print("The beans grow into rare deuce beans ★! The stinky smell grew a little, too.")
-								bag_space += 1
-								bag.append("deuce beans ★")
-								duece_beans = True
-							else:
+								print("You grow tall enough to play basketball. Unfortunately, your size gets you stuck until it wears off. Kind of a BIG problem. heh heh..")
+							if room_name == "marshes" or room_name == "cove" or room_name == "grove" or room_name == "fields":
 								bag.remove("growth potion")
 								print("You grow extraordinarily tall, but it's lonely up here. What would grandma think if she saw how fat you've gotten?")
+							if room_name == "grove":
+								bag.remove("growth potion")
+								print("You grow taller than the grove, tall enough to squish a bear! You can't see it in the trees, though.")
+							if room_name == "woods":
+								bag.remove("growth potion")
+								print("Giant butterflies are cool and all, but not when they eat all of your magic tomatoes!")
+								while "magic tomato" in bag:
+									bag.remove("magic tomato")
+							if room_name == "farms":
+								if deuce_beans == False:
+									bag.remove("growth potion")
+									print("The beans grow into rare deuce beans ★! The stinky smell grew a little, too.")
+									bag_space += 1
+									bag.append("deuce beans ★")
+									duece_beans = True
+								else:
+									bag.remove("growth potion")
+									print("You grow extraordinarily tall, but it's lonely up here. What would grandma think if she saw how fat you've gotten?")
 
-					if use_item == "water - breathing potion":
-						if room_name != "marshes" and room_name != "cove":
-							bag.remove("water - breathing potion")
-							print("Your respiratory system changes, and now no amount of water can stand in your way! ..if only there was some water nearby.")
-						if room_name == "marshes":
-							bag.remove("water - breathing potion")
-							print("You dive into the lake, ready to swim to the other side. Suddenly, a swamp monster kindly removes you from his territory. He also took your funky roots!")
-							for i in bag:
-								if i == "funky root":
+						if use_item == "water - breathing potion":
+							if room_name != "marshes" and room_name != "cove":
+								bag.remove("water - breathing potion")
+								print("Your respiratory system changes, and now no amount of water can stand in your way! ..if only there was some water nearby.")
+							if room_name == "marshes":
+								bag.remove("water - breathing potion")
+								print("You dive into the lake, ready to swim to the other side. Suddenly, a swamp monster kindly removes you from his territory. He also took your funky roots!")
+								while "funky root" in bag:
 									bag.remove("funky root")
-						if room_name == "cove":
-							if silver_fish == False:
-								bag.remove("water - breathing potion")
-								print("You dive underwater and encounter a silver fish ★! Is soft scales glitter in the deep.")
-								bag_space += 2
-								bag.append("silver fish ★")
-								silver_fish = True
-							else:
-								bag.remove("water - breathing potion")
-								print("Unfortunately, water breathing snails are a bit harder to come by, and there's nothing interesting underwater.")
+							if room_name == "cove":
+								if silver_fish == False:
+									bag.remove("water - breathing potion")
+									print("You dive underwater and encounter a silver fish ★! Is soft scales glitter in the deep.")
+									bag_space += 2
+									bag.append("silver fish ★")
+									silver_fish = True
+								else:
+									bag.remove("water - breathing potion")
+									print("Unfortunately, water breathing snails are a bit harder to come by, and there's nothing interesting underwater.")
 
-					if use_item == "hover potion":
-						if room_name == "tower" or room_name == "cave":
-							bag.remove("hover potion")
-							print("You float up off the ground.. You can fly! Up until you bonk your head on the hard ceiling, that is.")
-						if room_name == "fields" or room_name == "marshes" or room_name == "thicket" or room_name == "farms" or room_name == "cove" or room_name == "grove":
-							bag.remove("hover potion")
-							print("You hover in the air, but there's not very many potion ingredients up here. You crash land on your rump, most unhelpfully.")
-						if room_name == "woods":
-							if random_egg == False:
+						if use_item == "hover potion":
+							if room_name == "tower" or room_name == "cave":
 								bag.remove("hover potion")
-								print("You easily float up to the tree with the butterflies. Hey, look, a random egg ★! Luckily, this egg's absent single father is off getting milk or something.")
-								bag_space += 1
-								bag.append("random egg ★")
-								random_egg = True
-							else:
+								print("You float up off the ground.. You can fly! Up until you bonk your head on the hard ceiling, that is.")
+							if room_name == "fields" or room_name == "marshes" or room_name == "thicket" or room_name == "farms" or room_name == "cove" or room_name == "grove":
 								bag.remove("hover potion")
-								print("You lazily float up to the top of the tree, only to find an empty home. Shoot, now you're depressed or something.")
+								print("You hover in the air, but there's not very many potion ingredients up here. You crash land on your rump, most unhelpfully.")
+							if room_name == "woods":
+								if random_egg == False:
+									bag.remove("hover potion")
+									print("You easily float up to the tree with the butterflies. Hey, look, a random egg ★! Luckily, this egg's absent single father is off getting milk or something.")
+									bag_space += 1
+									bag.append("random egg ★")
+									random_egg = True
+								else:
+									bag.remove("hover potion")
+									print("You lazily float up to the top of the tree, only to find an empty home. Shoot, now you're depressed or something.")
 
-					if use_item == "invisibility potion":
-						if room_name != "grove":
-							bag.remove("invisibility potion")
-							print("Hey look, you're invisible! This would be the best prank ever, but there's no one around to spook!")
-						else:
-							if poohs_honey == False:
+						if use_item == "invisibility potion":
+							if room_name != "grove":
 								bag.remove("invisibility potion")
-								print("You stealthily creep past the bear. Luckily the bear has a stuffy nose and can't smell anything. You got pooh's honey ★!")
-								bag_space += 2
-								bag.append("pooh's honey ★")
-								poohs_honey = True
+								print("Hey look, you're invisible! This would be the best prank ever, but there's no one around to spook!")
 							else:
-								bag.remove("invisibility potion")
-								print("You're invisibile, but grandma wouldn't want you to take such a risk for nothing. Best to play it safe.")
+								if poohs_honey == False:
+									bag.remove("invisibility potion")
+									print("You stealthily creep past the bear. Luckily the bear has a stuffy nose and can't smell anything. You got pooh's honey ★!")
+									bag_space += 2
+									bag.append("pooh's honey ★")
+									poohs_honey = True
+								else:
+									bag.remove("invisibility potion")
+									print("You're invisibile, but grandma wouldn't want you to take such a risk for nothing. Best to play it safe.")
 
-					if use_item == "light potion":
-						if room_name != "cave":
-							bag.remove("light potion")
-							print("I said, oooooooooOOOOOOOOooooooooohhhhhhhhh I'm blinded by the lights~ Unfortunately, your glowing skin is most unhelpful.")
+						if use_item == "light potion":
+							if room_name != "cave" and room_name != "woods":
+								bag.remove("light potion")
+								print("I said, oooooooooOOOOOOOOooooooooohhhhhhhhh I'm blinded by the lights~ Unfortunately, your glowing skin is most unhelpful.")
 							if room_name == "woods":
 								bag.remove("light potion")
 								print("You glow defiantly under the shade of the trees. But your blinded butterflies are less impressed and leave you!")
-								for i in bag:
-									if i == "bubbly butterfly":
-										bag.remove("bubbly butterfly")
-						if room_name == "cave":
-							if crystal_shard == False:
-								bag.remove("light potion")
-								print("You easily illuminate the back of the cave, and spot a shiny crystal shard ★! It sparkles in the light.")
-								bag_space += 1
-								bag.append("crystal shard ★")
-								crystal_shard = True
-							else:
-								bag.remove("light potion")
-								print("A soft glow fills the cave, but there's nothing left in here. It's super interesting how boring this cave is.")
+								while "bubbly butterfly" in bag:
+									bag.remove("bubbly butterfly")
+							if room_name == "cave":
+								if crystal_shard == False:
+									bag.remove("light potion")
+									print("You easily illuminate the back of the cave, and spot a shiny crystal shard ★! It sparkles in the light.")
+									bag_space += 1
+									bag.append("crystal shard ★")
+									crystal_shard = True
+								else:
+									bag.remove("light potion")
+									print("A soft glow fills the cave, but there's nothing left in here. It's super interesting how boring this cave is.")
 
-					if use_item == "water - walking potion":
-						if room_name != "marshes":
-							bag.remove("water - walking potion")
-							print("You feel the power of christ flowing within you. You know you can walk on water, but there's no water around to prove it. Well shoot.")
+						if use_item == "water - walking potion":
+							if room_name != "marshes" and room_name != "cove":
+								bag.remove("water - walking potion")
+								print("You feel the power of christ flowing within you. You know you can walk on water, but there's no water around to prove it. Well shoot.")
 							if room_name == "cove":
 								bag.remove("water - walking potion")
 								print("You step across the water of the cove, feeling magical. Your seasick snails, on the other hand, don't. They're quick to abandon ship.")
-								for i in bag:
-									if i == "sus snail":
-										bag.remove("sus snail")
-						if room_name == "marshes":
-							if wisp_lily == False:
-								bag.remove("water - walking potion")
-								print("You cross the lake, thoroughly confusing some boaters as you pass. On the other side you find a wisp lily ★! I thought they were extinct.")
-								bag_space += 2
-								bag.append("wisp lily ★")
-								wisp_lily = True
-							else:
-								bag.remove("water - walking potion")
-								print("You cross the lake, but there's nothing left on this side of the swamp but an ogre you'd rather not annoy.")
+								while "sus snail" in bag:
+									bag.remove("sus snail")
+							if room_name == "marshes":
+								if wisp_lily == False:
+									bag.remove("water - walking potion")
+									print("You cross the lake, thoroughly confusing some boaters as you pass. On the other side you find a wisp lily ★! I thought they were extinct.")
+									bag_space += 2
+									bag.append("wisp lily ★")
+									wisp_lily = True
+								else:
+									bag.remove("water - walking potion")
+									print("You cross the lake, but there's nothing left on this side of the swamp but an ogre you'd rather not annoy.")
+					else:
+						print("That wasn't in your bag! (Maybe a spelling error?)")
 
 				if "★" in use_item:
 					print("You ought to save that for later.")
